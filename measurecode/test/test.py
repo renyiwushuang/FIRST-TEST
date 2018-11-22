5fd35231815a59081084bc7c8d7d84a2fb3dfb1a
YJZ <215242153@qq.com>
2018-11-22 17:14:31 +0800

    增加生产线数据分析代码

---
+++ measurecode/test/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 17:04:56 2018
+
+@author: Administrator
+"""
+
+import pandas
+import numpy
+fid = 'record (1).xlsx'
+df = pandas.read_excel(fid)
