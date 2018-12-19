f1bf307b1250e185ce0c731ca61fa497e298dd67
YJZ <215242153@qq.com>
2018-12-19 09:08:43 +0800

    完成判别功能

--- measurecode/test/getout.py
+++ measurecode/test/getout.py
@@ -45,12 +45,36 @@ while flag==0:
         
     else:
          flag = 1
+print(df['测试项'][1],'剔除坏值完成')        
 es = 0  
 if len(data)%2==0 :
       for i in range(0,int(len(data)/2-1)):
-          s = data[2*i]-data[2*i+1]
+          s = data[i]-data[i+int(len(data)/2)]
           es = s+es
-          print(s)
-          print(es)
+      print('样本数量为偶数')
+          #print('s is',s)
+          #print(es)
+else:
+    for i in range(0,int(len(data)/2)):
+          s = data[i]-data[i+int(len(data)/2+2)]
+          es = s+es
+    print('样本数量为奇数')
+if abs(es)>1 :
+    print('存在线性累积误差')
+else:
+    print('不存在线性累积误差')
+    
+          #print('s is',s)
+
+          #print(es)
+v =data-mean
+ab = 0
+for   i in range(0,len(data)-2):
+    ab = ab + v[i]*v[i+1]
+if abs(ab)-std*std*(len(data)-1)**0.5>0 :
+    print('存在周期性系统误差')
+else:
+    print('不存在周期性系统误差')
+      
 #print(len(data))    
  #print(getout)   
