4fde845bc4cad695ea9d2ba2b03dc2c7b61acfbe
YJZ <215242153@qq.com>
2019-03-05 17:13:52 +0800

    增加数据分析代码

--- measurecode/VISA/data analysis.py
+++ measurecode/VISA/data analysis.py
@@ -8,9 +8,14 @@ Created on Mon Mar  4 14:05:10 2019
 import matplotlib.pyplot as plt
 import numpy
 import collections
-data = numpy.loadtxt(r'E:\YJZ\work\EH100602A12\测试数据\动态功耗测试\GPS接收\1\photo.out')
+
+
+addr = r'E:\YJZ\work\EH100602A12\测试数据\动态功耗测试\ZGB\photo.out'
+data = numpy.loadtxt(addr)
 print(data)
 plt.plot(data,linewidth=0.02)
 dict1 = collections.Counter(data)
 list1= sorted(dict1.items(),key=lambda x:x[1])
 zero = list1[-1][0]      #基线确定
+numpy.save(addr,data)
+data2 = numpy.load(addr+'.npy')
