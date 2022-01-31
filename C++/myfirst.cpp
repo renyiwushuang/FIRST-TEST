c1c371fd2bb337c6d305e801b05451f1abce15c6
renyiwushuang <215242153@qq.com>
2022-01-31 10:05:24 +0800

    first

---
+++ C++/myfirst.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up and C++ me some time.";
+    cout << endl;
+    cout << "you won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
